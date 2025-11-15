34c3f337281eda3290c1da8e37bc0a65597afe96
tuht67khmt-ai <tu.ht.67khmt@ntu.edu.vn>
2025-11-15 21:16:33 +0700

    helloTU

---
+++ helloTU.py
@@ -0,0 +1,10 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> pint("Hello Huỳnh Tuấn Tú")
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    pint("Hello Huỳnh Tuấn Tú")
+NameError: name 'pint' is not defined. Did you mean: 'print'?
+>>> print("Hello Huỳnh Tuấn Tú")
+... 
+Hello Huỳnh Tuấn Tú
