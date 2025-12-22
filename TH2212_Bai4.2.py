7b8d2b37e948c78a10fe46c9fa2f0438da7a4a2c
tuht67khmt-ai <tu.ht.67khmt@ntu.edu.vn>
2025-12-22 15:14:38 +0700

    Create TH2212_Bai4.2.py
    tim so lon nhat va nho nhat dung def

---
+++ TH2212_Bai4.2.py
@@ -0,0 +1,18 @@
+a=float((input("nhap so a:")))
+b=float((input("nhap so b:")))
+c=float((input("nhap so c: ")))
+def so_lon_nhat(a,b,c):
+    if a>=b and a>=c:
+        return a
+    elif b>=a and b>=c:
+        return b
+    else:
+        return c
+print("so lon nhat la:",so_lon_nhat(a,b,c)) 
+def so_nho_nhat(a,b,c):
+    if a<=b and a<=c:
+        return a
+    elif b<=a and b<=c:
+        return b
+    else:
+        return c
